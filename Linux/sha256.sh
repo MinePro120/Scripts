88195e00126cf020316f46e93c6cd3e083601343
MinePro120 <37949795+MinePro120@users.noreply.github.com>
2020-01-29 13:29:52 +0200

    Updated sha256.sh

--- Linux/sha256.sh
+++ Linux/sha256.sh
@@ -7,5 +7,4 @@ rm SHA256SUMS.txt > /dev/null 2>&1
 echo "SHA256 :                                                          File :" >> /tmp/SHA256SUMS.txt
 find . -type f -exec sha256sum {} \; >> /tmp/SHA256SUMS.txt
 mv /tmp/SHA256SUMS.txt .
-
 exit 0
