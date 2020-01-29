40e8e601c783af15fb4b5c6dc9fa4e6c68468dcb
MinePro120 <37949795+MinePro120@users.noreply.github.com>
2020-01-29 13:39:07 +0200

    Added shutdown.py

---
+++ Raspberry Pi/shutdown.py
@@ -0,0 +1,18 @@
+from gpiozero import Button
+import time
+import os
+
+# While this script is running, you can safely shutdown the pi by
+# connecting GPIO 26 (pin 37) to ground (e.g. pin 39) for more than 1 second
+# Simple way to run as daemon : screen -dmS shutdown python shutdown.py
+# Simple way to run at startup : edit /etc/rc.local
+
+stopButton = Button (26)
+
+while True:
+     if stopButton.is_pressed:
+        time.sleep (1)
+        if stopButton.is_pressed:
+            os.system ("shutdown now -h")
+     time.sleep (1)
+exit (1)
