a0a55a75309f9946fae4a2474d8104b8b6a721c8
MinePro120 <37949795+MinePro120@users.noreply.github.com>
2020-01-29 13:22:22 +0200

    Added comment to sha256.sh

--- Linux/sha256.sh
+++ Linux/sha256.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Script that caclulates the sha256 checksums of all the
+# files in its directory and outputs them to SHA256SUMS.txt
+
 rm SHA256SUMS.txt > /dev/null 2>&1
 echo "SHA256 :                                                          File :" >> /tmp/SHA256SUMS.txt
 find . -type f -exec sha256sum {} \; >> /tmp/SHA256SUMS.txt
