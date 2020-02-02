2eb12d07a97baa997ff4a8024c5199243bbb5607
MinePro120 <37949795+MinePro120@users.noreply.github.com>
2020-02-02 19:29:41 +0200

    Removed header from sha256.sh

--- Linux/sha256.sh
+++ Linux/sha256.sh
@@ -4,8 +4,6 @@
 # files in its directory and outputs them to SHA256SUMS.txt
 
 rm SHA256SUMS.txt > /dev/null 2>&1
-echo "SHA256 :                                                          File :" >> /tmp/SHA256SUMS.txt
-echo >> /tmp/SHA256SUMS.txt
 find . -type f -exec sha256sum {} \; >> /tmp/SHA256SUMS.txt
 mv /tmp/SHA256SUMS.txt .
 exit 0
