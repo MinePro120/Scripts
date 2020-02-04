b24a19405ee200db6442dd66617713f97bf7e3d3
MinePro120 <37949795+MinePro120@users.noreply.github.com>
2020-02-05 00:59:23 +0200

    Added alphabetical sorting

--- Linux/sha256.sh
+++ Linux/sha256.sh
@@ -2,8 +2,10 @@
 
 # Script that caclulates the sha256 checksums of all the
 # files in its directory and outputs them to SHA256SUMS.txt
+# in alphabetical order (based on the file's directory)
 
 rm SHA256SUMS.txt > /dev/null 2>&1
 find . -type f -exec sha256sum {} \; >> /tmp/SHA256SUMS.txt
-mv /tmp/SHA256SUMS.txt .
+sort -k2 /tmp/SHA256SUMS.txt > SHA256SUMS.txt
+rm /tmp/SHA256SUMS.txt > /dev/null 2>&1
 exit 0
