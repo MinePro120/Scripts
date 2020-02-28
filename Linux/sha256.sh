e990610f03c3b5601ad72aec578ca299fef40344
MinePro120 <37949795+MinePro120@users.noreply.github.com>
2020-02-28 10:29:56 +0200

    Updated sha256.sh
    Excluded the hash of the script itself

--- Linux/sha256.sh
+++ Linux/sha256.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-# Script that caclulates the sha256 checksums of all the
-# files in its directory and outputs them to SHA256SUMS.txt
-# in alphabetical order (based on the file's directory)
+# Script that calculates the sha256 checksums of all the
+# files in its directory (excluding itself) and outputs
+# them to SHA256SUMS.txt in alphabetical order 
+# (based on the file's directory)
 
 rm SHA256SUMS.txt > /dev/null 2>&1
-find . -type f -exec sha256sum {} \; >> /tmp/SHA256SUMS.txt
+find . -type f -a ! -name 'sha256.sh' -exec sha256sum {} \; >> /tmp/SHA256SUMS.txt
 sort -k2 /tmp/SHA256SUMS.txt > SHA256SUMS.txt
 rm /tmp/SHA256SUMS.txt > /dev/null 2>&1
 exit 0
